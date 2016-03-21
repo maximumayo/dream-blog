241fb6da6c87d01ee39567aaf51b23d765eb8553
maximumayo <maxgmayo@gmail.com>
2016-03-21 15:15:12 -0700

    added angular route cdn #2

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <!--ANGULAR-->
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-route.min.js"></script>
     <!--CUSTOM CSS-->
     <link rel="stylesheet" href="style.css">
 
