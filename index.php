3e1940b4968b1020595cc34bdf5432a6828d6417
maximumayo <maxgmayo@gmail.com>
2016-03-21 15:12:00 -0700

    basic files #2

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Fluffy Chainsaw Blog</title>
+
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <!--ANGULAR-->
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
+    <!--CUSTOM CSS-->
+    <link rel="stylesheet" href="style.css">
+
+
+</head>
+<body>
+
+
+</body>
+</html>
