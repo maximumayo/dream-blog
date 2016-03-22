8220950cde288e82e8291bd5d80add40276e3482
stan wielga <svwielga4@gmail.com>
2016-03-21 21:48:55 -0700

    testing out ajax for get and create

---
+++ stans_test_page.php
@@ -0,0 +1,31 @@
+<?php
+$added = Date("Y-m-d H:i:s");
+echo $added;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Fluffy Chainsaw Blog</title>
+
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <!--ANGULAR-->
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.8/angular-ui-router.min.js"></script>
+    <!--CUSTOM CSS-->
+    <link rel="stylesheet" href="style.css">
+    <script src="stan.js"/>
+
+
+</head>
+<body>
+<input id='title' placeholder="title goes here" /><br><br>
+<textarea id='article' rows='20' cols='50' placeholder="story goes here"></textarea><br><br>
+<button onclick="postNewBlogArticle(<?=$added?>, $('#article').val(), $('#title').val())">Submit</button>
+
+
+
+</body>
+</html>
