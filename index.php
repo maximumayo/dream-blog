04c4b0d343b92987a97a7cdbf92ea49d4303874b
Timothy Nguyen <shimoshixp@gmail.com>
2016-03-21 19:56:09 -0700

    commenting

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Fluffy Chainsaw Blog</title>
-
-
+    <!--JQuery and Bootstrap-->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <!--ANGULAR-->
@@ -12,13 +11,11 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.8/angular-ui-router.min.js"></script>
     <!--CUSTOM CSS-->
     <link rel="stylesheet" href="style.css">
+    <!--CUSTOM JAVASCRIPT-->
     <script src="ui-router.js"></script>
-
 
 </head>
 <body ng-app="blogApp">
 <div ui-view></div>
-<p>Hi</p>
-
 </body>
 </html>
