a8b52ef50d46458ca199aaeeb1c2ee7222acfa0a
stan wielga <svwielga4@gmail.com>
2016-03-22 12:58:23 -0700

    sanatized inputs

--- operations/deleteUser.php
+++ operations/deleteUser.php
@@ -1,7 +1,7 @@
 <?php
 //require("mysql_connect.php");
-$username = $_POST['username'];
-$password = sha1($_POST['password']);
+$username = trim(filter_input(INPUT_POST,'username',FILTER_SANITIZE_STRING));
+$password = sha1(trim(filter_input(INPUT_POST,'password',FILTER_SANITIZE_STRING)));
 $data = ['success'=> false];
 $query = "DELETE FROM  `users` WHERE  `username` =  '$username'";
 $result = mysqli_query($conn, $query);
