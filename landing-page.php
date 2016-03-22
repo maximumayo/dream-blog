00af6a3d8a557b35425855577d3901dee61b9200
maximumayo <maxgmayo@gmail.com>
2016-03-21 19:34:46 -0700

    header, inputs, buttons

---
+++ landing-page.php
@@ -0,0 +1,10 @@
+<div class="jumbotron text-center">
+
+<h1>Fluffy Chainsaw Blog</h1>
+    <input type="text" placeholder="email">
+    <input type="password" placeholder="password">
+    <input type="submit" value="Log In">
+    <input type="submit" value="Sign Up">
+
+
+</div>
