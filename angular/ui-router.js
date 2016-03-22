13291a85ddbc3bfe6cf5ece5ea42a9359e89854f
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-03-22 11:50:02 -0700

    Put flies in appropriate folders - Frank

---
+++ angular/ui-router.js
@@ -0,0 +1,28 @@
+
+
+blog.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/landing');
+
+    $stateProvider
+
+        .state('landing', {
+            url: '/landing',
+            templateUrl: 'templates/landing-page.php'
+        })
+
+        .state('registration', {
+            url: '/registration',
+            templateUrl: 'templates/registration-page.php'
+        })
+
+        .state('newsfeed', {
+            url: '/newsfeed',
+            templateUrl: 'templates/news-feed.php'
+        })
+
+        .state('profile', {
+            url: '/profile',
+            templateUrl: 'templates/profile-page.php'
+        })
+
+});
