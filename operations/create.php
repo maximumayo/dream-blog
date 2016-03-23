9f9852c9cffea1533a05be55b833db32c6c8f89e
stan wielga <svwielga4@gmail.com>
2016-03-23 07:44:45 -0700

    resolved #51, #38, #57,

--- operations/create.php
+++ operations/create.php
@@ -1,9 +1,9 @@
 <?php
 //require("mysql_connect.php");
 $article = trim(filter_input(INPUT_POST,'article',FILTER_SANITIZE_STRING));
-$added = trim(filter_input(INPUT_POST,'added',FILTER_SANITIZE_STRING));
+$added = Date("Y-m-d H:i:s");
 $title = trim(filter_input(INPUT_POST,'title',FILTER_SANITIZE_STRING));
-$user_id = 1;
+$user_id = trim(filter_input(INPUT_POST,'user_id',FILTER_SANITIZE_NUMBER_INT));
 $query = "insert into `posts` set `added`='$added', `article`='$article', `title`='$title', `users_id`='$user_id'";
 $output = ['success' => false];
 $result = mysqli_query($conn,$query);
