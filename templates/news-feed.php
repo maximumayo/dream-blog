ae6557e6bedfac110b98375470d051d9b6d2c39a
Trisha Meyer <tmeye62@gmail.com>
2016-03-22 17:03:36 -0700

    changes

--- templates/news-feed.php
+++ templates/news-feed.php
@@ -1,14 +1,3 @@
-<!--<div class="jumbotron text-center">-->
-<!---->
-<!--    <h1>Home</h1>-->
-<!---->
-<!--    <a ui-sref="profile"><button class="btn btn-default" type="submit">My Profile</button></a>-->
-<!---->
-<!--    <button ui-sref="landing" type="submit" class="btn btn-danger">Log Out</button>-->
-<!---->
-<!---->
-<!---->
-<!--</div>-->
 <div class='bgimg container-fluid'>
     <nav class="navbar" id='navbar'>
         <div class="container-fluid">
