9aad84aece416ccb051e48954428b99498967477
Timothy Nguyen <shimoshixp@gmail.com>
2016-03-23 10:30:25 -0700

    pull max

--- templates/registration-page.php
+++ templates/registration-page.php
@@ -3,12 +3,12 @@
 
     <h1>Create An Account</h1>
     <input type="text" placeholder="UserName" ng-model="accountInfo.userName" required> <br>
-    <input type="text" placeholder="First Name" ng-model="accountInfo.firstName"> <br>
-    <input type="text" placeholder="Last Name" ng-model="accountInfo.lastName"> <br>
-    <input type="password" placeholder="Password" ng-model="accountInfo.password"> <br>
-    <input type="password" placeholder="Confirm Password" ng-model="accountInfo.passwordConfirm"> <br>
-    <input type="email" placeholder="Email" ng-model="accountInfo.email"> <br>
-    <input type="number" placeholder="Phone Number" ng-model="accountInfo.phoneNumber"> <br>
+    <input type="text" placeholder="First Name" ng-model="accountInfo.firstName" required> <br>
+    <input type="text" placeholder="Last Name" ng-model="accountInfo.lastName" required> <br>
+    <input type="password" placeholder="Password" ng-model="accountInfo.password" required> <br>
+    <input type="password" placeholder="Confirm Password" ng-model="accountInfo.passwordConfirm" required> <br>
+    <input type="email" placeholder="Email" ng-model="accountInfo.email" required> <br>
+    <input type="number" placeholder="Phone Number" ng-model="accountInfo.phoneNumber" required> <br>
 
     <a ng-click="createAccount()" ui-sref="landing" type="submit" class="btn btn-success">Create Account</a>
 
