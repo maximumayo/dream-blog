2f5c442bb606c60ca5c9edae2819b815af104849
maximumayo <maxgmayo@gmail.com>
2016-03-23 10:30:15 -0700

    #52 added regex to sign in form

--- templates/registration-page.php
+++ templates/registration-page.php
@@ -2,15 +2,41 @@
     <!--    ng-controller="signupController as sc"-->
 
     <h1>Create An Account</h1>
-    <input type="text" placeholder="UserName" ng-model="accountInfo.userName" required> <br>
-    <input type="text" placeholder="First Name" ng-model="accountInfo.firstName"> <br>
-    <input type="text" placeholder="Last Name" ng-model="accountInfo.lastName"> <br>
-    <input type="password" placeholder="Password" ng-model="accountInfo.password"> <br>
-    <input type="password" placeholder="Confirm Password" ng-model="accountInfo.passwordConfirm"> <br>
-    <input type="email" placeholder="Email" ng-model="accountInfo.email"> <br>
-    <input type="number" placeholder="Phone Number" ng-model="accountInfo.phoneNumber"> <br>
 
-    <a ng-click="createAccount()" ui-sref="landing" type="submit" class="btn btn-success">Create Account</a>
+    <form name="create">
+
+        <input type="text" placeholder="User Name" ng-model="accountInfo.userName" pattern="[a-zA-Z0-9]{3,12}" required>
+        <br>
+
+        <input type="text" placeholder="First Name" ng-model="accountInfo.firstName" pattern="[a-zA-Z]{1,20}" required>
+        <br>
+
+        <input type="text" placeholder="Last Name" ng-model="accountInfo.lastName" pattern="[a-zA-Z]{1,20}" required>
+        <br>
+
+        <input type="password" placeholder="Password" ng-model="accountInfo.password"
+               pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*^[a-zA-Z]([a-zA-Z0-9]){11,31}\b" required>
+        <br>
+
+        <input type="password" placeholder="Confirm Password" ng-model="accountInfo.passwordConfirm" required>
+        <br>
+
+        <input type="email" placeholder="Email" ng-model="accountInfo.email"
+               pattern="^[a-zA-Z0-9]+.?[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+|[a-zA-Z0-9]@[a-zA-Z0-9]\.[a-zA-Z0-9]"
+               required> <br>
+
+        <input type="text" placeholder="Phone Number" ng-model="accountInfo.phoneNumber"
+               pattern="1?\(?[0-9]{3}\)?-? *[0-9]{3}-? *[0-9]{4}\b" required> <br>
+
+        <input type="submit" ng-click="createAccount() ui-sref=" landing" class="btn btn-success" value="Create
+        Account">
+
+
+    </form>
 
 
 </div>
+
+
+
+
