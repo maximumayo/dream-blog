3c0b58b35f5c3fb445a96d1035860707123b9eac
Trisha Meyer <tmeye62@gmail.com>
2016-03-24 12:47:17 -0700

    adding pictures to posts on home page but if the photo is not present it shows an empty box

--- angular/angular.js
+++ angular/angular.js
@@ -2,21 +2,5 @@ var blog = angular.module('blogApp', ['ui.router']);
 
 
 blog.value('user_id', null);
-//
-//blog.directive('updateDirective', function(){
-//    return {
-//        restrict: 'AE',
-//        templateUrl: 'templates/profile-page.php',
-//        scope: {
-//            profilePosts: '=',
-//            updateData: '&'
-//        },
-//        link: function(scope, elem, attr){
-//            elem.find('#newPostbtn').bind('click', function(){
-//               scope.$apply(function(){
-//                   scope.updateData();
-//               });
-//            });
-//        }
-//    }
-//});
+
+
