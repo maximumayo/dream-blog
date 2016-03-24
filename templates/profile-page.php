ed4fef85aac1d1cc2bf260b60e8db8abf4b62d68
Trisha Meyer <tmeye62@gmail.com>
2016-03-24 16:17:27 -0700

    added css to profile pic

--- templates/profile-page.php
+++ templates/profile-page.php
@@ -104,7 +104,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" id='newPostbtn' class="btn btn-primary" data-dismiss="modal"
+                    <button type="button" class="btn btn-primary" data-dismiss="modal"
                             ng-click="ppc.newPost()">Save
                         changes
                     </button>
